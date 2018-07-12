513934e267c1af10bc58f8b073c8d380e5c7ca98
Evg-Ko <31292566+9420128@users.noreply.github.com>
2018-07-12 22:18:34 +0400

    popup

--- src/js/app.js
+++ src/js/app.js
@@ -23,7 +23,31 @@ $(document).ready(function(){
       $(this).toggleClass('nav__icon-drop-menu-active');
       $(this).parent().next('.drop-menu').toggleClass('drop-menu-active');
     });
-  
+
+    $('.js-single-item').slick({
+      autoplay: true,
+      autoplaySpeed: 8000,
+    });
+
+     $('.js-single-item-2').slick({
+      autoplay: true,
+      autoplaySpeed: 8000,
+    });
+
+  /* ----------Popup-----------*/
+
+$('#js-topL-popup').magnificPopup({
+     items: [
+      {
+        src: '<div class="js-topL-modal"><h3>Заявка на замер</h3><form><label></label><input type="text" name="text" placeholder="Ваше имя" required><br><label></label><input type="tel" name="phone" placeholder="Ваш номер телефона" required><br><label></label><textarea name="textarea" placeholder="Введите здесь адрес или оставьте сообщение" required></textarea><hr><small>Оставьте заявку или свяжитесь с нами по телефону: <a href="/+78129420128">+7812 942 01 28</a><br>(Мы работаем в будние дни с 10 до 20 часов.)</small><button type="button" class="btn-def">Отправитить</button></form></div>',
+        type: 'inline'
+      }
+    ],
+    
+    type: 'image' // this is default type
+  });
+
+
 
 });
-//menu.slideToggle();
+
